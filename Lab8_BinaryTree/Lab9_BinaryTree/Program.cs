b1bbdca5e21bb716e176b0810a183f175c7349db
AidanMack99 <50879271+AidanMack99@users.noreply.github.com>
2019-05-21 16:46:17 +0100

    Add files via upload

---
+++ Lab8_BinaryTree/Lab9_BinaryTree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9_BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> root = new Node<int>(6);
+            root.Left = new Node<int>(2);
+            root.Left.Right = new Node<int>(5);
+            BinTree<int> tree = new BinTree<int>(root);
+
+            string buffer = "";
+            tree.InOrder(ref buffer);
+            System.Console.WriteLine(buffer);
+            Console.ReadKey();
+        }
+    }
+}
