ddaf8ec859ff7e828ba74fdfc25f2890f12aa1e7
AidanMack99 <50879271+AidanMack99@users.noreply.github.com>
2019-05-21 16:41:19 +0100

    Add files via upload

---
+++ Lab6_Libary/Lab6_Libary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_Libary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>();
+            Book book1 = new Book("1234BN", "Moby Dick");
+            Book book2 = new Book("5647BN", "Ullysses");
+            books.Add(book1);
+            books.Add(book2);
+            foreach (Book b in books)
+                Console.WriteLine(b.ISBN+ " " + b.Title);
+            Console.ReadKey();
+        }
+    }
+}
